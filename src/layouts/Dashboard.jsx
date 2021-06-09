7892f3e1f9377b076c098ede8ea560c343a896e2
ruveyharuzgar <ruveyharuzgar.108@gmail.com>
2021-06-10 00:07:04 +0300

    login-exit,cart,product details and react-router

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -3,17 +3,23 @@ import ProductList from "../pages/ProductList";
 import Categories from "./Categories";
 import Navi from "./Navi";
 import { Grid } from "semantic-ui-react";
+import { Route } from "react-router";
+import ProductDetail from "../pages/ProductDetail";
+import CartDetail from "../pages/CartDetail";
 
 export default function Dashboard() {
   return (
     <div>
-      <Grid >
+      <Grid>
         <Grid.Row>
-          <Grid.Column width={4} >
+          <Grid.Column width={4}>
             <Categories />
           </Grid.Column>
-          <Grid.Column width={12} >
-            <ProductList />
+          <Grid.Column width={12}>
+            <Route exact path="/" component={ProductList} />
+            <Route exact path="/products" component={ProductList} />
+            <Route path="/products/:name" component={ProductDetail} />
+            <Route path="/cart" component={CartDetail} />
           </Grid.Column>
         </Grid.Row>
       </Grid>
