560c06b1f04d98189155e99eb82c87e6793d0959
ruveyharuzgar <ruveyharuzgar.108@gmail.com>
2021-06-17 00:28:39 +0300

    redux

--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -1,22 +1,24 @@
 import React, { useState } from "react";
 import CartSummary from "./CartSummary";
-import { Button, Container, Menu } from "semantic-ui-react";
+import { Container, Menu } from "semantic-ui-react";
 import SignedOut from "./SignedOut";
 import SignedIn from "./SignedIn";
 import { useHistory } from "react-router";
+import { useSelector } from "react-redux";
 
 export default function Navi() {
+  const { cartItems } = useSelector((state) => state.cart);
   const [isAuthenticated, setIsAuthenticated] = useState(true);
 
-  const history=useHistory()
+  const history = useHistory();
 
   function handleSignOut() {
-    setIsAuthenticated(false)
-    history.push("/")
+    setIsAuthenticated(false);
+    history.push("/");
   }
 
   function handleSignIn() {
-    setIsAuthenticated(true)
+    setIsAuthenticated(true);
   }
   return (
     <div>
@@ -26,8 +28,12 @@ export default function Navi() {
           <Menu.Item name="messages" />
 
           <Menu.Menu position="right">
-            <CartSummary />
-            {isAuthenticated ? <SignedIn signOut={handleSignOut} bisey="1"/> : <SignedOut signIn={handleSignIn}/>}
+            {cartItems.length > 0 && <CartSummary />}
+            {isAuthenticated ? (
+              <SignedIn signOut={handleSignOut} bisey="1" />
+            ) : (
+              <SignedOut signIn={handleSignIn} />
+            )}
           </Menu.Menu>
         </Container>
       </Menu>
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import { Icon, Label, Menu, Table } from "semantic-ui-react";
+import { Button, Icon, Menu, Table } from "semantic-ui-react";
 import ProductService from "../services/productService";
+import { addToCart } from "../store/actions/cartActions";
+import { toast } from "react-toastify"
 
 export default function ProductList() {
+  const dispatch = useDispatch();
+
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
@@ -13,6 +18,10 @@ export default function ProductList() {
       .then((result) => setProducts(result.data.data), []);
   });
 
+  const handleAddToCart = (product) => {
+    dispatch(addToCart(product));
+    toast.success(`${product.productName} sepete eklendi`)
+  };
   return (
     <div>
       <Table celled>
@@ -23,6 +32,7 @@ export default function ProductList() {
             <Table.HeaderCell>Stok Adedi</Table.HeaderCell>
             <Table.HeaderCell>Açıklama</Table.HeaderCell>
             <Table.HeaderCell>Kategori</Table.HeaderCell>
+            <Table.HeaderCell></Table.HeaderCell>
           </Table.Row>
         </Table.Header>
 
@@ -38,6 +48,9 @@ export default function ProductList() {
               <Table.Cell>{product.unitsInStock}</Table.Cell>
               <Table.Cell>{product.quantityPerUnit}</Table.Cell>
               <Table.Cell>{product.category.categoryName}</Table.Cell>
+              <Table.Cell>
+                <Button onClick={()=>handleAddToCart(product)}> Sepete Ekle</Button>
+              </Table.Cell>
             </Table.Row>
           ))}
         </Table.Body>
